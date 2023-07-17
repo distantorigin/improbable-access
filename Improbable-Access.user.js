3b9b87168159fe7ad7e42f873b14449a6394cdaf
distantorigin <distantorigin@distantorig.in>
2023-07-17 18:05:57 -0500

    Initial commit

---
+++ Improbable-Access.user.js
@@ -0,0 +1,113 @@
+// ==UserScript==
+// @name         Improbable Access
+// @namespace   https://distantorig.in/
+// @description Makes the Improbable Island interface more accessible for screen readers.
+// @include     http://*improbableisland.com/*
+// @include     https://*improbableisland.com/*
+// @exclude     http://*improbableisland.com/home.php*
+// @exclude     https://*improbableisland.com/home.php*
+// @version     0.1.0
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // Turn groups of nav links into lists.
+
+    // Find all navsections and loop through each one
+    var navSections = document.querySelectorAll('.navsection');
+    
+    navSections.forEach(function(navSection) {
+        
+        // Find the navhead element
+        var navHead = navSection.previousElementSibling;
+
+        // Set navhead to aria-hidden
+        navHead.setAttribute('aria-hidden', 'true');
+
+        // Create a new ul element
+        var newList = document.createElement('ul');
+
+        // Set aria-label for the ul element using the content of navhead
+        newList.setAttribute('aria-label', navHead.textContent.trim());
+
+        // Find all links inside the current navsection and iterate through each
+     
+        var navItems = navSection.querySelectorAll('.nav');
+
+        navItems.forEach(function(navItem) {
+
+            var newListItem = document.createElement('li');
+
+            newListItem.appendChild(navItem);
+            newList.appendChild(newListItem);
+
+        });
+
+        // Insert the ul element before the navsection
+        navSection.parentNode.insertBefore(newList, navSection);
+    });
+
+    // Add headings for each potential navigation. (In addition to the regions that already exist. Sue me.)
+
+    var navigationDivs = document.querySelectorAll('div.navigation');
+
+    // Loop through each navigation div
+    navigationDivs.forEach(function(navigationDiv) {
+
+        // Create a new h2 element for each navigation
+        var newHeading = document.createElement('h2');
+
+        newHeading.textContent = "Navigation";
+
+        // Apply CSS to make the h2 element invisible
+        newHeading.style.position = 'absolute';
+        newHeading.style.width = '1px';
+        newHeading.style.height = '1px';
+        newHeading.style.margin = '-1px';
+        newHeading.style.border = '0';
+        newHeading.style.padding = '0';
+        newHeading.style.overflow = 'hidden';
+        newHeading.style.clip = 'rect(0 0 0 0)';
+        newHeading.style.clipPath = 'inset(50%)';
+        newHeading.style.whiteSpace = 'nowrap';
+
+        // Insert the h2 element before the navigation div
+        navigationDiv.parentNode.insertBefore(newHeading, navigationDiv);
+    });
+
+    // Find chat headers and turn them into fake headings for screen readers
+
+    const chatHeaderDivs = document.querySelectorAll('div.ChatHeader');
+
+    // Loop through each div element
+    chatHeaderDivs.forEach(function(chatDiv) {
+        // Add the role and level attributes
+        chatDiv.setAttribute('role', 'heading');
+        chatDiv.setAttribute('level', '2');
+    });
+
+    // Add labels for chat inputs
+
+    const chatInputs = document.querySelectorAll('input[name="comment"]');
+
+    chatInputs.forEach(function(chatInput) {
+
+        var label = document.createElement("label");
+
+        label.setAttribute("for", chatInput.id);
+        label.innerText = "Comment:";
+        chatInput.parentNode.insertBefore(label, chatInput);
+    });
+
+    // Check for the presence of a combat card and add remaining HP to the title of the page
+
+    const combatCardHpDiv = document.querySelector('.combatCard_hp');
+  
+    if (combatCardHpDiv) {
+        
+        const hpValue = combatCardHpDiv.querySelector('span').textContent;
+    
+        // Update the title of the page with current HP
+        document.title += ' [' + hpValue + ' HP]';
+    }
+})();
---
+++ README.md
@@ -0,0 +1,28 @@
+# improbable-access
+
+Contrary to what one might believe, a userscript to improve the screen reader accessibility of Improbable Island.
+
+To install this userscript, you will need a userscript manager, such as [https://www.tampermonkey.net/](Tampermonkey) or [https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/](Greasemonkey \(Firefox only\))
+
+Once you've installed the corresponding web extension, you can click below to install Improbable Access in your browser. This link will also work in-place for script updates.
+
+[Install Improbable Access](https://github.com/distantorigin/improbable-access/raw/main/Improbable-Access.user.js)
+
+## Features
+
+The following is an exhaustive but potentially incomplete list of features introduced by Improbable Access:
+
+* Add a heading before navigation sections on all pages for quickly getting to actionable items.
+* Enclose all navigation sections in unordered lists.
+* Hide navigation headers from screen readers and replace them with aria-labels, resulting in the label appearing at the top of the list rather than on its own line.
+* Add headings for chat (if present).
+* Properly label the chat input field.
+* Add current hitpoints to the page title when in battle.
+
+## Disclaimer
+
+I am, by any stretch of the imagination, quite new to Improbable Island. As a result, I disclaim myself from anything listed below:
+
+1. Not developing features that might make sense later on in the game. I'm going to build the script as I continue playing, which means a lot of it might unintentionally be written for a newbie's ideal gameplay, not an older player.
+2. I don't know what's going on. I'm new. That's it. If I do something dumb, that's why. Please tell me and I'll try to fix or patch the script as soon as I can.
+3. Abandoning the script. I've been playing Improbable Island for two whole days, so I dare say I may move on, get tired of it, get bored, or a combination of all three. Feel free to fork Improbable Access and make your own updates as needed.
